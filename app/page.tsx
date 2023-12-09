ed6745b2bec5bec718cf9a7eda6d4183598e5b14
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-09 16:15:55 +0500

    Added MaskText component into main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import { MaskText } from "@animation";
+
 export default function App() {
-	return <div>App</div>;
+	const phrases = ["Chase Your", "Dream with us"];
+
+	return (
+		<div className="w-full h-screen flex items-center justify-center">
+			<h1 className="text-[30px] uppercase font-bold">
+				<MaskText>{phrases}</MaskText>
+			</h1>
+		</div>
+	);
 }
